f64ee3b77056eeaab6e71d0c8cc992189553bf68
Martijn <mpkeesmaat@gmail.com>
2019-11-07 14:53:20 +0100

    fix object count query

--- script.js
+++ script.js
@@ -3,22 +3,24 @@ const nCategories = 19;
 const categories = [];
 
 const queryMainCategories = `
-    #+ summary: Get titles meestvoorkomende catogrieen
-    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-    PREFIX dc: <http://purl.org/dc/elements/1.1/>
-    PREFIX dct: <http://purl.org/dc/terms/>
-    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-    PREFIX edm: <http://www.europeana.eu/schemas/edm/>
-    PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-    SELECT ?category  ?categoryLabel (COUNT(?categoryLabel) AS ?objCount) WHERE {
-      ?cho edm:isRelatedTo <https://hdl.handle.net/20.500.11840/termmaster2802> .
-      <https://hdl.handle.net/20.500.11840/termmaster2802> skos:narrower ?category .
-      ?category skos:prefLabel ?categoryLabel .
-    } 
-
-    GROUP BY ?categoryLabel ?category
-    ORDER BY DESC(?objCount)
-  `;
+  #+ summary: Get titles meestvoorkomende catogrieen
+  PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+  PREFIX dc: <http://purl.org/dc/elements/1.1/>
+  PREFIX dct: <http://purl.org/dc/terms/>
+  PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+  PREFIX edm: <http://www.europeana.eu/schemas/edm/>
+  PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+  # tel aantallen per materiaal
+  SELECT ?categoryLabel ?category (COUNT(?allChos) AS ?objCount) WHERE {
+    ?cho edm:isRelatedTo <https://hdl.handle.net/20.500.11840/termmaster2802> .
+    <https://hdl.handle.net/20.500.11840/termmaster2802> skos:narrower ?category .
+    ?category skos:prefLabel ?categoryLabel .
+    ?category skos:narrower* ?allChos .
+  }
+    
+  GROUP BY ?categoryLabel ?category
+  ORDER BY DESC(?objCount)
+`;
 
 const fetchMaterialPerCategory = (
   querySrc,
@@ -56,26 +58,26 @@ const getCategoriesFromData = data => {
 const fetchMaterialPerCategoryEach = categoriesTermaster => {
   categoriesTermaster.forEach(category => {
     const queryCategories = `
-      PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-      PREFIX dc: <http://purl.org/dc/elements/1.1/>
-      PREFIX dct: <http://purl.org/dc/terms/>
-      PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-      PREFIX edm: <http://www.europeana.eu/schemas/edm/>
-      PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-      # tel aantallen per materiaal
-      SELECT ?subcategorie ?materiaalLabel (COUNT(?cho) AS ?choCount) WHERE {
-      # haal van een term in de thesaurus de subcategorieen op
-      ${category.termmaster} skos:narrower* ?subcategorie .
-      # haal de objecten van deze subcategorieen en het materiaal
-      ?cho edm:isRelatedTo ?subcategorie .
-      ?cho dct:medium ?materiaal .
-      # haal het Label op van materiaal
-      ?materiaal skos:prefLabel ?materiaalLabel .
-      }
-      GROUP BY ?subcategorie ?materiaalLabel
-      ORDER BY DESC(?choCount)
-      LIMIT 5
-    `;
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX dc: <http://purl.org/dc/elements/1.1/>
+        PREFIX dct: <http://purl.org/dc/terms/>
+        PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+        PREFIX edm: <http://www.europeana.eu/schemas/edm/>
+        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+        # tel aantallen per materiaal
+        SELECT ?subcategorie ?materiaalLabel (COUNT(?cho) AS ?choCount) WHERE {
+        # haal van een term in de thesaurus de subcategorieen op
+        ${category.termmaster} skos:narrower* ?subcategorie .
+        # haal de objecten van deze subcategorieen en het materiaal
+        ?cho edm:isRelatedTo ?subcategorie .
+        ?cho dct:medium ?materiaal .
+        # haal het Label op van materiaal
+        ?materiaal skos:prefLabel ?materiaalLabel .
+        }
+        GROUP BY ?subcategorie ?materiaalLabel
+        ORDER BY DESC(?choCount)
+        LIMIT 5
+      `;
 
     fetchMaterialPerCategory(
       'https://api.data.netwerkdigitaalerfgoed.nl/datasets/ivo/NMVW/services/NMVW-20/sparql',
@@ -95,7 +97,8 @@ const handleFetchMaterialPerCategory = (data, category) => {
 const normalizeMaterialPerCategory = (data, category) => {
   return {
     name: category.name,
-    material: data.results.bindings.map(i => {
+    value: Number(category.value),
+    materials: data.results.bindings.map(i => {
       return {
         name: i.materiaalLabel.value,
         value: Number(i.choCount.value)
