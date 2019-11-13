f15a07bc1c14349c9dda9791ec90340a99e68f2a
Martijn <mpkeesmaat@gmail.com>
2019-11-13 15:48:40 +0100

    fix axes bar chart

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -194,20 +194,14 @@ function renderBarChart(categories, width, height) {
   var padding = 50;
   var svg = addGlobalSVGBarChart(width, height);
   var xScale = addXScaleBarChart(width, padding, categories);
-  addBarsToBarChart(xScale, svg, categories);
   addLabelsToBarChart(svg, categories);
   addXAxisToBarChart(svg, height, padding, xScale);
-  addGridlinesToBarChart(svg, width, height);
+  addGridlinesToBarChart(svg, width, height, xScale);
+  addBarsToBarChart(xScale, svg, categories);
 }
 
 var addGlobalSVGBarChart = function addGlobalSVGBarChart(width, height) {
   return d3.select(".bar-chart").append("svg").attr("width", width).attr("height", height);
-};
-
-var addXScaleBarChart = function addXScaleBarChart(width, padding, categories) {
-  return d3.scaleLinear().domain([0, d3.max(categories, function (d) {
-    return d.value;
-  })]).range([padding, width - padding]);
 };
 
 var addBarsToBarChart = function addBarsToBarChart(xScale, svg, categories) {
@@ -217,7 +211,7 @@ var addBarsToBarChart = function addBarsToBarChart(xScale, svg, categories) {
     return i * 50;
   }).attr("width", function (d) {
     return xScale(d.value) - 50;
-  }).attr("height", 15).attr("class", "bar").attr("rx", 15 / 2); //height / 2
+  }).attr("height", 15).attr("class", "bar"); // .attr("rx", 15 / 2) //height / 2
 };
 
 var addLabelsToBarChart = function addLabelsToBarChart(svg, categories) {
@@ -231,21 +225,27 @@ var addLabelsToBarChart = function addLabelsToBarChart(svg, categories) {
   .attr("text-anchor", "end").attr("transform", "translate(90," + 0 + ")").call(_helpers.wrap, 100);
 };
 
-var addXAxisToBarChart = function addXAxisToBarChart(svg, height, padding, xScale, categories) {
-  var xAxis = d3.axisBottom(xScale).ticks(3);
-  svg.append("g").attr("transform", "translate(55," + (height - padding) + ")").attr("color", '#9AA1A9').call(xAxis).call(function (g) {
+var addXScaleBarChart = function addXScaleBarChart(width, padding, categories) {
+  return d3.scaleLinear().domain([0, d3.max(categories, function (d) {
+    return d.value;
+  })]).range([padding, width - padding]);
+};
+
+var addXAxisToBarChart = function addXAxisToBarChart(svg, height, padding, xScale) {
+  var xAxis = d3.axisBottom(xScale).ticks(4);
+  svg.append("g").attr("transform", "translate(50," + (height - padding) + ")").attr("color", '#9AA1A9').attr("class", "x-axis").call(xAxis).call(function (g) {
     return g.select(".domain").remove();
   });
 };
 
-var addGridlinesToBarChart = function addGridlinesToBarChart(svg, width, height) {
-  var x = d3.scaleTime().range([0, width]);
-  svg.append("g").attr("class", "grid").attr("transform", "translate(100," + height + ")").call(makeXGridlines(x).tickSize(-height).tickFormat(""));
+var addGridlinesToBarChart = function addGridlinesToBarChart(svg, width, height, xScale) {
+  var x = xScale;
+  svg.append("g").attr("class", "grid").attr("transform", "translate(50," + (height - 45) + ")").attr("stroke", "#E9EBF1").call(makeXGridlines(x).tickSize(-height).tickFormat(""));
 }; // https://bl.ocks.org/d3noob/c506ac45617cf9ed39337f99f8511218
 
 
 var makeXGridlines = function makeXGridlines(x) {
-  return d3.axisBottom(x).ticks(5);
+  return d3.axisBottom(x).ticks(4);
 };
 },{"./helpers":"helpers.js"}],"renderDonutChart.js":[function(require,module,exports) {
 "use strict";
--- src/renderBarChart.js
+++ src/renderBarChart.js
@@ -4,10 +4,10 @@ export default function renderBarChart(categories, width, height) {
   const padding = 50;
   const svg = addGlobalSVGBarChart(width, height);
   const xScale = addXScaleBarChart(width, padding, categories);
-  addBarsToBarChart(xScale, svg, categories);
   addLabelsToBarChart(svg, categories);
   addXAxisToBarChart(svg, height, padding, xScale);
-  addGridlinesToBarChart(svg, width, height)
+  addGridlinesToBarChart(svg, width, height, xScale)
+  addBarsToBarChart(xScale, svg, categories);
 }
 
 const addGlobalSVGBarChart = (width, height) => {
@@ -16,13 +16,6 @@ const addGlobalSVGBarChart = (width, height) => {
     .append("svg")
     .attr("width", width)
     .attr("height", height);
-}
-
-const addXScaleBarChart = (width, padding, categories) => {
-  return d3
-    .scaleLinear()
-    .domain([0, d3.max(categories, d => d.value)])
-    .range([padding, width - padding]);
 }
 
 const addBarsToBarChart = (xScale, svg, categories) => {
@@ -36,7 +29,7 @@ const addBarsToBarChart = (xScale, svg, categories) => {
     .attr("width", d => xScale(d.value) - 50)
     .attr("height", 15)
     .attr("class", "bar")
-    .attr("rx", 15 / 2) //height / 2
+  // .attr("rx", 15 / 2) //height / 2
 }
 
 const addLabelsToBarChart = (svg, categories) => {
@@ -55,21 +48,30 @@ const addLabelsToBarChart = (svg, categories) => {
     .call(wrap, 100);
 }
 
-const addXAxisToBarChart = (svg, height, padding, xScale, categories) => {
-  const xAxis = d3.axisBottom(xScale).ticks(3);
+const addXScaleBarChart = (width, padding, categories) => {
+  return d3
+    .scaleLinear()
+    .domain([0, d3.max(categories, d => d.value)])
+    .range([padding, width - padding]);
+}
+
+const addXAxisToBarChart = (svg, height, padding, xScale) => {
+  const xAxis = d3.axisBottom(xScale).ticks(4);
   svg.append("g")
-    .attr("transform", "translate(55," + (height - padding) + ")")
+    .attr("transform", "translate(50," + (height - padding) + ")")
     .attr("color", '#9AA1A9')
+    .attr("class", "x-axis")
     .call(xAxis)
     .call(g => g.select(".domain").remove())
 }
 
-const addGridlinesToBarChart = (svg, width, height) => {
-  const x = d3.scaleTime().range([0, width]);
+const addGridlinesToBarChart = (svg, width, height, xScale) => {
+  const x = xScale;
 
   svg.append("g")
     .attr("class", "grid")
-    .attr("transform", "translate(100," + height + ")")
+    .attr("transform", "translate(50," + (height - 45) + ")")
+    .attr("stroke", "#E9EBF1")
     .call(makeXGridlines(x)
       .tickSize(-height)
       .tickFormat("")
@@ -77,5 +79,5 @@ const addGridlinesToBarChart = (svg, width, height) => {
 }
 
 // https://bl.ocks.org/d3noob/c506ac45617cf9ed39337f99f8511218
-const makeXGridlines = x => d3.axisBottom(x).ticks(5)
+const makeXGridlines = x => d3.axisBottom(x).ticks(4)
 
